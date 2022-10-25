29387ccc4757396497c35b0abcb7532f11797cff
Jose Luiz Fagundes <jlfagundes@gmail.com>
2022-10-25 10:02:56 -0300

    criando novos endpoints com CLI

--- src/app.service.ts
+++ src/app.service.ts
@@ -3,7 +3,7 @@ import { Injectable } from '@nestjs/common';
 @Injectable()
 export class AppService {
   getHello(): string {
-    return 'Hello World!';
+    return 'Bem vindo a API Avanade';
   }
   getLogin(): string {
     return 'Aqui virá o login do usuário';
