cc48819946ed013de1a98a8e74bb5373f2460ab7
Jose Luiz Fagundes <jlfagundes@gmail.com>
2022-10-25 11:16:07 -0300

    injeção de dependencia no auth

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,4 +1,18 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Post } from '@nestjs/common';
+import { AuthService } from './auth.service';
 
 @Controller('auth')
-export class AuthController {}
+export class AuthController {
+    constructor(private readonly authService: AuthService) {}
+
+    @Post('/login')
+    async authLogin(@Body() req) {
+
+        const {login, password} = req;
+        console.log('login: ', login);
+        console.log('senha: ', password);
+        
+        
+        return this.authService.authLogin(login, password);
+    };
+};
--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,4 +1,10 @@
 import { Injectable } from '@nestjs/common';
 
 @Injectable()
-export class AuthService {}
+export class AuthService {
+    async authLogin(login: string, password: string){
+        console.log('No service login: ', login);
+        console.log('No service senha: ', password);
+        return {login, password};
+    }
+}
